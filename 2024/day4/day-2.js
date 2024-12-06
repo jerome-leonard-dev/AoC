7fa432580c6a793cd73c07f1314a44894f74cc69
jerome.leonard <jerome.leonard.mail@gmail.com>
2024-12-06 20:54:13 +0100

    day4-2

--- 2024/day4/day-2.js
+++ 2024/day4/day-2.js
@@ -6,30 +6,22 @@ fs.readFile('input', 'utf8', (err, data) => {
     return;
   }
 
-  let lns = [];
-  let cols = [];
-  let diags = [];
-  let diags2 = [];
-  const lines = data.split('\n');
-  for( let j = 0; j < lines.length; j++){
-      const line = lines[j];
-      lns.push(line);
-      for( let i = 0; i < line.length; i++){
-        cols[i] = (cols[i]==undefined?"":cols[i])+line[i];
-         diags[lines.length-j+i] = (diags[lines.length-j+i]==undefined?"":diags[lines.length-j+i])+line[i];
-         diags2[lines.length-j+i] = (diags2[lines.length-j+i]==undefined?"":diags2[lines.length-j+i])+line[line.length-i-1];
+  let total = 0;
+  const lines = data.replace(/ /g,'').split('\n');
+  for( let j = 1; j < lines.length-1; j++){
+      for( let i = 1; i < lines[j].length-1; i++){
+        if( lines[j][i]=='A'){
+          let str=j+":"+i;
+          if( ((lines[j-1][i-1]==='M' && lines[j+1][i+1]==='S') || (lines[j-1][i-1]==='S' && lines[j+1][i+1]==='M')) &&
+              ((lines[j+1][i-1]==='M' && lines[j-1][i+1]==='S') || (lines[j+1][i-1]==='S' && lines[j-1][i+1]==='M'))){
+            total++;
+            str+="X";
+          }
+          console.log(str);
+        }
+
       }
   };
-
-  let total = 0;
-  const full = lns.concat(cols, diags, diags2);
-  for( let i = 0; i < full.length; i++){
-    if(full[i]){
-      console.log( full[i] + "  => "+[...full[i].matchAll('XMAS')].length + ":" +[...full[i].matchAll('SAMX')].length);
-      total += [...full[i].matchAll('XMAS')].length;
-      total += [...full[i].matchAll('SAMX')].length;
-    }
-  }
 
   console.log("Total: "+total);
 });
