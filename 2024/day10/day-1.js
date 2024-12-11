b31ab8c8b4939edda5d9e9560487fb41cc62fa4a
jerome leonard <jerome.leonard@cgi.com>
2024-12-11 12:50:16 +0100

    day10

--- 2024/day10/day-1.js
+++ 2024/day10/day-1.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 
-const paths = [];
-
-fs.readFile('test', 'utf8', (err, data) => {
+fs.readFile('input', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
@@ -18,13 +16,14 @@ fs.readFile('test', 'utf8', (err, data) => {
   }
   dump(map);
 
+  const paths = [];
   for( let y = 0; y < map.length; y++){
     for( let x = 0; x < map[y].length; x++){
       if( map[y][x] == 0){
         const start = {x:x, y:y};
-        const startId = ""+(start.x*10000000000+start.y);
+        const startId = ""+(start.x+"x"+start.y);
         paths[startId] = [];
-        moveNext(map, start, start);
+        moveNext(map, paths, start, start);
       }
     }
   }
@@ -33,38 +32,38 @@ fs.readFile('test', 'utf8', (err, data) => {
 
   let total = 0;
   for(let i in paths){
-    total += paths[i].length;
+    total += dedoublonne(paths[i]).length;
   }
 
   console.log("Total: "+total);
 
 });
 
-function moveNext(map, start, current){
+function moveNext(map, paths, start, current){
   const val = map[current.y][current.x];
   
-  console.log( "moveNext => " + JSON.stringify(current)+"   :  "+val);
+  console.log( "".padStart(val, "  ") + JSON.stringify(current)+"   :  "+val);
   if(val == 9){
-    const startId = ""+(start.x*10000000000+start.y);
+    const startId = ""+(start.x+"x"+start.y);
     paths[startId].push(current);
     console.log(JSON.stringify(start)+"==>"+JSON.stringify(current))
     return;
   }
   //N :  0,-1
   if( current.y > 0 && map[current.y-1][current.x]==map[current.y][current.x]+1){
-    moveNext(map, start, {x:current.x, y: current.y-1});
+    moveNext(map, paths, start, {x:current.x, y: current.y-1});
   }
   //S :  0, 1
   if( current.y < map.length-1 && map[current.y+1][current.x]==map[current.y][current.x]+1){
-    moveNext(map, start, {x:current.x, y: current.y+1});
+    moveNext(map, paths, start, {x:current.x, y: current.y+1});
   }
   //E :  1, 0
-  if( current.x > 0 && map[current.y][current.x-1]==map[current.y][current.x]+1){
-    moveNext(map, start, {x:current.x-1, y: current.y});
+  if( current.x < map[0].length-1 && map[current.y][current.x+1]==map[current.y][current.x]+1){
+    moveNext(map, paths, start, {x:current.x+1, y: current.y});
   }
-  //W : -1, 0
-  if( current.x > map[0].length-1 && map[current.y][current.x+1]==map[current.y][current.x]+1){
-    moveNext(map, start, {x:current.x+1, y: current.y});
+  //W : -1, 0 
+  if( current.x > 0 && map[current.y][current.x-1]==map[current.y][current.x]+1){
+    moveNext(map, paths, start, {x:current.x-1, y: current.y});
   }
 }
 
@@ -72,4 +71,8 @@ function dump(tab){
   for( let i in tab){
     console.log( tab[i].join(' '));
   }
+}
+
+function dedoublonne(items) {
+  return items.filter((o, index, arr) => arr.findIndex(item => item.x === o.x && item.y === o.y) === index);
 }
--- 2024/day10/day-2.js
+++ 2024/day10/day-2.js
@@ -5,92 +5,74 @@ fs.readFile('input', 'utf8', (err, data) => {
     console.error(err);
     return;
   }
+  const map = [];
 
-  let res = [];
-  console.log(data);
-  const values = data.split('');
-  for( let i = 0; i < values.length; i++){
-    if(i%2 == 0){
-      res.push({id:i/2, length:values[i]*1});
-    }else{
-      res.push({id:-1, length:values[i]*1});
+  const values = data.split('\n');
+  for( let y = 0; y < values.length; y++){
+    map[y] = [];
+    for( let x = 0; x < values[y].length; x++){
+      map[y][x] = values[y][x]*1;
     }
   }
-  //dump(res);
+  dump(map);
 
-  let end = res[res.length-1].id;
-  while(end >0){
-    const item = res.find(x=>x.id==end);
-    res = moveOne(res, item);
-    //if(item.id!=-1)dump(res, "    ");
-    end = end-1;
-  }
-  //dump(res);
-  
-  let total = 0;
-  let idx = 0;
-  for(let i = 0 ;i<res.length;i++){
-    const count = res[i].length;
-    const id = res[i].id;
-    for(j = 0; j < count;j++){
-      if( res[i].id != -1){
-        total += id*idx;
+  const paths = [];
+  for( let y = 0; y < map.length; y++){
+    for( let x = 0; x < map[y].length; x++){
+      if( map[y][x] == 0){
+        const start = {x:x, y:y};
+        const startId = ""+(start.x+"x"+start.y);
+        paths[startId] = [];
+        moveNext(map, paths, start, start);
       }
-      idx++;
     }
+  }
+
+  console.log("Paths: "+JSON.stringify(paths));
+
+  let total = 0;
+  for(let i in paths){
+    total += (paths[i]).length;
   }
 
   console.log("Total: "+total);
 
 });
 
-function moveOne( list, item){
-  let res = [];
-  let fill = false;
-  let moved = false;
-  for(let i = 0; i < list.length;i++){
-    if( fill){
-      res.push(list[i]);
-      continue;
-    }
-    switch(list[i].id){
-      case -1:
-        if(!moved && list[i].length == item.length){
-          res.push(item);
-          moved = true;
-        } else if(!moved && list[i].length >= item.length){
-          res.push(item);
-          res.push({id:-1, length:(list[i].length - item.length)});
-          moved = true;
-        } else {
-          res.push(list[i]);
-        }
-        break;
-      case item.id:
-        if(!moved){
-          res.push(list[i]);
-        } else {
-          res.push({id:-1, length:item.length});
-        }
-        fill = true;
-        break;
-      default:
-        res.push(list[i]);
-        break
-    }
+function moveNext(map, paths, start, current){
+  const val = map[current.y][current.x];
+  
+  console.log( "".padStart(val, "  ") + JSON.stringify(current)+"   :  "+val);
+  if(val == 9){
+    const startId = ""+(start.x+"x"+start.y);
+    paths[startId].push(current);
+    console.log(JSON.stringify(start)+"==>"+JSON.stringify(current))
+    return;
   }
-
-  return res;
+  //N :  0,-1
+  if( current.y > 0 && map[current.y-1][current.x]==map[current.y][current.x]+1){
+    moveNext(map, paths, start, {x:current.x, y: current.y-1});
+  }
+  //S :  0, 1
+  if( current.y < map.length-1 && map[current.y+1][current.x]==map[current.y][current.x]+1){
+    moveNext(map, paths, start, {x:current.x, y: current.y+1});
+  }
+  //E :  1, 0
+  if( current.x < map[0].length-1 && map[current.y][current.x+1]==map[current.y][current.x]+1){
+    moveNext(map, paths, start, {x:current.x+1, y: current.y});
+  }
+  //W : -1, 0 
+  if( current.x > 0 && map[current.y][current.x-1]==map[current.y][current.x]+1){
+    moveNext(map, paths, start, {x:current.x-1, y: current.y});
+  }
 }
 
-function dump(list, prefix=""){
-  let res = "";
-  for(let i = 0;i<list.length;i++){
-    if( list[i].id==-1){
-      res += "".padStart(list[i].length,'.');
-    }else{
-      res += "".padStart(list[i].length,""+list[i].id);
-    }
+function dump(tab){
+  for( let i in tab){
+    console.log( tab[i].join(' '));
   }
-  console.log(prefix+res);
+}
+
+function dedoublonne(items) {
+  return items.filter((o, index, arr) => arr.findIndex(item => item.x === o.x && item.y === o.y) === index);
 }
