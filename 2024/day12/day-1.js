c28269ac589fa8819b6fb8b7e242a05f2809c9fd
jerome.leonard <jerome.leonard.mail@gmail.com>
2024-12-23 19:23:23 +0100

    day12

--- 2024/day12/day-1.js
+++ 2024/day12/day-1.js
@@ -1,40 +1,86 @@
-const fs = require('fs');
-console.log(new Date().toString());
-fs.readFile('input', 'utf8', (err, data) => {
+function main(input = 'input', num = ''){
+  const fs = require('fs');
+  console.log(new Date().toString());
+  fs.readFile(input+num, 'utf8', async (err, data) => {
   
-  if (err) {
-    console.error(err);
-    return;
+  console.log(new Date().toString());
+/*
+  RRRRIICCFF
+  RRRRIICCCF
+  VVRRRCCFFF
+  VVRCCCJFFF
+  VVVVCJJCFE
+  VVIVCCJJEE
+  VVIIICJJEE
+  MIIIIIJJEE
+  MIIISIJEEE
+  MMMISSJEEE
+*/
+  let total=0;
+  let map = [];
+  let lines = data.split('\n');
+  for( let i = 0; i < lines.length; i++){
+    map[map.length] = lines[i].split('');
+  }
+  let map_o = JSON.parse(JSON.stringify(map));
+
+  let zones = [];
+
+  for(let y = 0; y < map.length; y++){
+    for(let x = 0; x < map[y].length; x++){
+      if( map[y][x] != '.'){
+        if(zones[map[y][x]] == undefined)zones[map[y][x]]=[];
+        let garden = [];
+        propage(map, y, x, garden);
+        zones[map_o[y][x]][zones[map_o[y][x]].length]=garden;
+      }
+    }
   }
 
-  //125 17
-  let stones = data.split(' ').map(x=>x*1);
-  for( let i = 0; i < 25; i++){
-    let tmp = [];
-    //console.log(stones);
-    for( j in stones){
-      tmp = tmp.concat(blinkStone(stones[j]));
-    };
-    console.log ( (""+i).padStart(4,' ')+" : "+(""+tmp.length).padStart(10,' '));//+" : "+tmp);
-    stones = tmp;
+  let nb = 1;
+  for( let type in zones){
+    for( let idx in zones[type]){
+      let surface = 0;
+      let perimetre = 0;
+      for(let i = 0; i < zones[type][idx].length; i++){
+        surface+=1;
+        perimetre+=getPerimetre(map_o, zones[type][idx][i].x, zones[type][idx][i].y);
+      }
+      console.log(nb+++":"+type+"=>"+surface+"*"+perimetre+"="+surface*perimetre);
+      total+=(surface*perimetre);
+    }
   }
 
-  console.log("Total: "+stones.length);
+  console.log("Total: "+total);
   console.log(new Date().toString());
 });
 
-function blinkStone(value){
-  let res = []
-  if( value == 0){
-    res = [1]
-  } else if( (""+value).length%2==0){
-    svalue = ""+value;
-    res =  [svalue.slice(0,svalue.length/2)*1,svalue.slice(svalue.length/2)*1];
 
-  } else {
-    res = [value*2024];
-  }
-  //console.log(value + " => " + res);
-  return res;
+}
+
+main(process.argv[2], process.argv[3]);
+
+function propage(map, y, x, zones){
+  zones[zones.length]={x:x,y:y};
+  const type = map[y][x];
+  map[y][x]='*';
+  if( map[y-1] != undefined && map[y-1][x] == type)propage(map, y-1, x, zones);
+  if( map[y+1] != undefined && map[y+1][x] == type)propage(map, y+1, x, zones);
+  if( map[y][x-1] != undefined && map[y][x-1] == type)propage(map, y, x-1, zones);
+  if( map[y][x+1] != undefined && map[y][x+1] == type)propage(map, y, x+1, zones);
+//  console.log('\n\n'+map.map(x=>x.join(' ')).join('\n'));
+  map[y][x]='.';
+  return zones;
+}
+
+function getPerimetre(map, x, y){
+  let perimetre = 0;
+  const type = map[y][x];
+  if( map[y-1] == undefined || map[y-1][x] == undefined || map[y-1][x] != type)perimetre+=1;
+  if( map[y+1] == undefined || map[y+1][x] == undefined || map[y+1][x] != type)perimetre+=1;
+  if( map[y][x-1] == undefined || map[y][x-1] != type)perimetre+=1;
+  if( map[y][x]+1 == undefined || map[y][x+1] != type)perimetre+=1;
+
+  return perimetre;
 }
 
--- 2024/day12/day-2.js
+++ 2024/day12/day-2.js
@@ -2,80 +2,118 @@ function main(input = 'input', num = ''){
   const fs = require('fs');
   console.log(new Date().toString());
   fs.readFile(input+num, 'utf8', async (err, data) => {
-    
-    if (err) {
-      console.error(err);
-      return;
-    }
+  
+  console.log(new Date().toString());
+/*
+  RRRRIICCFF
+  RRRRIICCCF
+  VVRRRCCFFF
+  VVRCCCJFFF
+  VVVVCJJCFE
+  VVIVCCJJEE
+  VVIIICJJEE
+  MIIIIIJJEE
+  MIIISIJEEE
+  MMMISSJEEE
+*/
+  let total=0;
+  let map = [];
+  let lines = data.split('\n');
+  for( let i = 0; i < lines.length; i++){
+    map[map.length] = lines[i].split('');
+  }
+  let map_o = JSON.parse(JSON.stringify(map));
 
-    //125 17
-    let stones = data.split(' ').map(x=>BigInt(x));
-    for( let i = 0; i < 75; i++){
-      const start = Date.now();
-      let tmp = await processData(stones);
+  let zones = [];
 
-      console.log ((""+(Date.now()-start)).padStart(10,"0")+":"+ (""+i).padStart(4,' ')+" : "+(""+tmp.length).padStart(20,' ') +" : "+new Date().toString());
-      console.log(tmp.join(' '));
-      stones = tmp;
-    }
-
-
-    const batchSize = 100000; // Taille d'un lot
-    let i = 0;
-    for (; i*batchSize < stones.length; i++) {
-      try {
-        fs.writeFileSync('int'+(i+1), stones.slice(i*batchSize, (i +1)*batchSize).join(' '));
-      } catch (err) {
-        console.error(err);
+  for(let y = 0; y < map.length; y++){
+    for(let x = 0; x < map[y].length; x++){
+      if( map[y][x] != '.'){
+        if(zones[map[y][x]] == undefined)zones[map[y][x]]=[];
+        let garden = [];
+        propage(map, y, x, garden);
+        zones[map_o[y][x]][zones[map_o[y][x]].length]=garden;
       }
     }
-
-
-    console.log("Total: "+stones.length);
-    console.log("Chuncks: "+i);
-    console.log(new Date().toString());
-    return i;
-  });
-
-  const blinkStones = (values) => {
-    let result = [];
-    for( i = 0; i < values.length; i++){
-      result.push(blinkStone(values[i]));
-    }
-    return result;
   }
 
-  const blinkStone = (value) => {
-    const start = Date.now();
-    let res = []
-    if( value == 0){
-      res = [BigInt(1)]
-    } else if( (""+value).length%2==0){
-      svalue = ""+value;
-      res =  [BigInt(svalue.slice(0,svalue.length/2)),BigInt(svalue.slice(svalue.length/2))];
-
-    } else {
-      res = [value*BigInt(2024)];
+  let nb = 1;
+  let total2 = 0;
+  for( let type in zones){
+    for( let idx in zones[type]){
+      let surface = 0;
+      let perimetre = 0;
+      let corners = 0;
+      let map_a = JSON.parse(JSON.stringify(map));
+      for(let i = 0; i < zones[type][idx].length; i++){
+        surface+=1;
+        perimetre+=getPerimetre(map_o, zones[type][idx][i].x, zones[type][idx][i].y);
+        corners+= getCorner(map_o, zones[type][idx][i].x, zones[type][idx][i].y);
+        map_a[zones[type][idx][i].y][zones[type][idx][i].x]=getCorner(map_o, zones[type][idx][i].x, zones[type][idx][i].y);
+      }
+      console.log(nb+++":"+type+"=>"+surface+"*"+perimetre+"="+surface*perimetre+"=>"+surface+"*"+corners+"="+surface*corners);
+//      console.log(map_a.map(x=>x.join(' ')).join('\n')+'\n');
+      total+=(surface*perimetre);
+      total2+=(surface*corners);
     }
-    //console.log("=="+(Date.now()-start));
-    //console.log(value + " => " + res);
-    return res;
   }
 
-  const processData = async (data) => {
-    const batchSize = 5000; // Taille d'un lot
-    const batches = [];
-    
-    for (let i = 0; i < data.length; i += batchSize) {
-        batches.push(data.slice(i, i + batchSize));
-    }
-    
-    const promises = batches.map((batch) => blinkStones(batch));
-    const results = await Promise.all(promises);
-    return results.flat().flat(); // Combine les résultats
-  };
+  console.log("Total: "+total);
+  console.log("Total2: "+total2);
+  console.log(new Date().toString());
+});
+
+
 }
 
 main(process.argv[2], process.argv[3]);
 
+function propage(map, y, x, zones){
+  zones[zones.length]={x:x,y:y};
+  const type = map[y][x];
+  map[y][x]='*';
+  if( map[y-1] != undefined && map[y-1][x] == type)propage(map, y-1, x, zones);
+  if( map[y+1] != undefined && map[y+1][x] == type)propage(map, y+1, x, zones);
+  if( map[y][x-1] != undefined && map[y][x-1] == type)propage(map, y, x-1, zones);
+  if( map[y][x+1] != undefined && map[y][x+1] == type)propage(map, y, x+1, zones);
+//  console.log('\n\n'+map.map(x=>x.join(' ')).join('\n'));
+  map[y][x]='.';
+  return zones;
+}
+
+function getPerimetre(map, x, y){
+  let perimetre = 0;
+  const type = map[y][x];
+  if( map[y-1] == undefined || map[y-1][x] == undefined || map[y-1][x] != type)perimetre+=1;
+  if( map[y+1] == undefined || map[y+1][x] == undefined || map[y+1][x] != type)perimetre+=1;
+  if( map[y][x-1] == undefined || map[y][x-1] != type)perimetre+=1;
+  if( map[y][x]+1 == undefined || map[y][x+1] != type)perimetre+=1;
+
+  return perimetre;
+}
+
+function getCorner(map, x, y){
+  let corners = 0;
+
+  const type = map[y][x];
+  const N = (map[y-1] == undefined || map[y-1][x] == undefined)?'.':map[y-1][x];
+  const S = (map[y+1] == undefined || map[y+1][x] == undefined)?'.':map[y+1][x];
+  const W = (map[y] == undefined || map[y][x-1] == undefined)?'.':map[y][x-1];
+  const E = (map[y] == undefined || map[y][x+1] == undefined)?'.':map[y][x+1];
+  const NE = (map[y-1] == undefined || map[y-1][x+1] == undefined)?'.':map[y-1][x+1];
+  const NW = (map[y-1] == undefined || map[y-1][x-1] == undefined)?'.':map[y-1][x-1];
+  const SE = (map[y+1] == undefined || map[y+1][x+1] == undefined)?'.':map[y+1][x+1];
+  const SW = (map[y+1] == undefined || map[y+1][x-1] == undefined)?'.':map[y+1][x-1];
+  
+
+  if(N!=type && E != type)corners++;
+  if(E!=type && S != type)corners++;
+  if(S!=type && W != type)corners++;
+  if(W!=type && N != type)corners++;
+  if(N==type && E == type && NE != type)corners++;
+  if(N==type && W == type && NW != type)corners++;
+  if(S==type && E == type && SE != type)corners++;
+  if(S==type && W == type && SW != type)corners++;  
+  return corners;
+}
 
